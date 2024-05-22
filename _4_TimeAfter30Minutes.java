3d9696590d74e66f8bbb50129e6ccf35a28e9d90
Detelinka <158742307+detelinka@users.noreply.github.com>
2024-05-22 19:58:37 +0300

    Add files via upload

---
+++ _4_TimeAfter30Minutes.java
@@ -0,0 +1,24 @@
+package JavaBasicsRevision.LAB;
+
+import java.util.Scanner;
+
+public class _4_TimeAfter30Minutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hours = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        int totalMinutes = (hours * 60 + minutes) + 30;    //Превръщаме всичко в минути и добавяме 15
+
+        //Пресмятаме времето - часове : минути
+        int newHours = totalMinutes / 60;
+        int newMinutes = totalMinutes % 60;
+
+        if (newHours >= 24){
+            newHours = 0;
+            System.out.printf("%d:%02d", newHours, newMinutes);
+        } else{
+            System.out.printf("%d:%02d", newHours, newMinutes);
+        }
+    }
+}
